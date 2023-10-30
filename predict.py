ca113fd109ce53c3d1ca10951e7af773c1b5cf68
Александр <kalyrginwot@mail.ru>
2023-10-30 21:15:23 +0500

    test

--- predict.py
+++ predict.py
@@ -1,7 +1,17 @@
+# import cv2
+# from ultralytics import YOLO
+# img_pth = "4.jpg"
+# model = YOLO("best.pt")
+# results = model(source=img_pth)
+# res_plotted = results[0].plot()
+# cv2.imshow("result", res_plotted)
+# cv2.waitKey(0)
+
+
 import cv2
 from ultralytics import YOLO
-img_pth = "4.jpg"
-model = YOLO("best.pt") 
+img_pth = "1.jpeg"
+model = YOLO("./car_type/best.pt")
 results = model(source=img_pth)
 res_plotted = results[0].plot()
 cv2.imshow("result", res_plotted)
